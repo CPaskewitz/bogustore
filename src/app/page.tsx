4bd6fd0db4b4b533123b4c04e8091338b31f8aa1
Corey Paskewitz <cpaskewitz@coreys-mbp.home>
2024-08-26 13:38:18 -0400

    add promotion and categorylist to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
+import Promotion from "@/components/Promotion";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8 bg-beige-100">
+      <Promotion />
+      <CategoryList />
       <ProductList />
    </main>
   );
