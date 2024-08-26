b53a938d7f11557da8d8a0416feb545411f3752b
Corey Paskewitz <cpaskewitz@coreys-mbp.home>
2024-08-26 13:19:00 -0400

    add product list to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-
+import ProductList from "@/components/ProductList";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8 bg-beige-100">
-    </main>
+      <ProductList />
+   </main>
   );
 }
