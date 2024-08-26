3344a8396dcba3e4d4723d99251b78d19be7a2d6
Corey Paskewitz <cpaskewitz@coreys-mbp.home>
2024-08-26 13:10:04 -0400

    create footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="p-6 bg-beige-300 text-center text-brown-600">
+            © 2024 Homey Store - All rights reserved
+        </footer>
+    );
+}
