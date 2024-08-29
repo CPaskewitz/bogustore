dc59aa1ef2a942360bf885949394476de4f02603
Corey Paskewitz <cpaskewitz@Coreys-MacBook-Pro.local>
2024-08-29 14:46:09 -0400

    fix page layout

--- src/app/sale/page.tsx
+++ src/app/sale/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import ProductCard from '../components/ProductCard';
+import ProductCard from '../../components/ProductCard';
 
 type Product = {
     id: number;
