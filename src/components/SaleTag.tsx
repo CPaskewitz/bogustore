4f2d9fee0388e70443c23ed55fc53fca24abe96c
Corey Paskewitz <cpaskewitz@Coreys-MacBook-Pro.local>
2024-08-29 13:48:49 -0400

    create sale tag component

---
+++ src/components/SaleTag.tsx
@@ -0,0 +1,7 @@
+export default function SaleTag() {
+    return (
+        <span className="absolute top-2 left-2 bg-soft-coral text-white text-xs font-bold px-2 py-1 rounded">
+            50% OFF
+        </span>
+    );
+}
