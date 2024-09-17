bdf90bc633c2ce6f1817ed5d3f42ac01a37f0f9d
Corey Paskewitz <corey.paskewitz@gmail.com>
2024-09-17 17:04:07 -0400

    fix verticle alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,26 +5,28 @@ import CartIcon from '../components/CartIcon';
 export default function Header() {
   return (
     <header className="p-6 bg-beige-200 shadow-md">
-      <nav className="max-w-6xl mx-auto flex justify-center items-center">
+      <nav className="max-w-6xl mx-auto flex items-center">
         <Link href="/" className="text-3xl font-bold text-brown-800 mr-auto">
           BoguStore
         </Link>
-        <ul className="flex gap-8 text-brown-700">
-          <li>
+        <ul className="flex items-center gap-8 text-brown-700">
+          <li className="flex items-center">
             <Link href="/" className="hover:text-brown-800 transition">
               Home
             </Link>
           </li>
-          <li>
+          <li className="flex items-center">
             <Link href="/sale" className="hover:text-brown-800 transition">
               Sale
             </Link>
           </li>
           <li className="flex items-center">
-            <Link href="/cart" className="hover:text-brown-800 transition ml-2">
+            <Link href="/cart" className="hover:text-brown-800 transition flex items-center">
               Cart
+              <span className="ml-2">
+                <CartIcon />
+              </span>
             </Link>
-            <CartIcon />
           </li>
         </ul>
       </nav>
