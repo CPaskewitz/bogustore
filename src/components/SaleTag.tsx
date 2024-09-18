b9b16adb0f05ce9c208280422f64f83d97d6ef4d
Corey Paskewitz <corey.paskewitz@gmail.com>
2024-09-18 12:29:35 -0400

    make sale tag dynamic

--- src/components/SaleTag.tsx
+++ src/components/SaleTag.tsx
@@ -1,7 +1,7 @@
-export default function SaleTag() {
+export default function SaleTag({ discount }: { discount: number }) {
     return (
         <span className="absolute top-2 right-2 bg-soft-coral text-white text-lg font-bold px-3 py-2 rounded">
-            50% OFF
+            {discount}% OFF
         </span>
     );
 }
