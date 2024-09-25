2a3d0fd5655688583812d632047c8b7bd9fc3816
Corey Paskewitz <corey.paskewitz@gmail.com>
2024-09-25 18:41:09 -0400

    add id to select

--- src/components/SizeDropDown.tsx
+++ src/components/SizeDropDown.tsx
@@ -11,6 +11,7 @@ export default function SizeDropdown({ sizes, selectedSize, onSizeChange }: Size
         <div className="mb-4">
             <label className="block text-brown-800 font-semibold">Select Size:</label>
             <select
+                id="size-select"
                 value={selectedSize || ''}
                 onChange={(e) => onSizeChange(e.target.value)}
                 className="border px-3 py-2 rounded-lg w-full text-brown-800"
