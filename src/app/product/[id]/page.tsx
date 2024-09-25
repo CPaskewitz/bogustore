f5622d196388235c9ddef2e80829609ce74cb270
Corey Paskewitz <corey.paskewitz@gmail.com>
2024-09-25 18:18:34 -0400

    import react for testing

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ProductImage from '../../../components/ProductImage';
